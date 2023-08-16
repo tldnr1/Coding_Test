6b2a90e1fe80a44a5322f10172013de0477916fb
tldnr <tldnr1@knu.ac.kr>
2023-08-16 19:24:48 +0900

    위에서 아래로 내려오는 풀이 추가  - 최선의 경우를 경로 저장하듯이 탐색하는 풀이를 다들 더 많이 사용하는 것 같다  - 아마 내가 한 풀이가 백트래킹(?) 인 것 같다

--- Baekjoon/DP/1932.py
+++ Baekjoon/DP/1932.py
@@ -15,3 +15,28 @@ for i in range(n-2, -1, -1):
         graph[i][j] += max(graph[i+1][j], graph[i+1][j+1])
 
 print(graph[0][0])
+
+
+'''
+위에서 아래로 내려오는 풀이 : https://animoto1.tistory.com/entry/%EB%B0%B1%EC%A4%80-1932-%EC%A0%95%EC%88%98-%EC%82%BC%EA%B0%81%ED%98%95-%ED%8C%8C%EC%9D%B4%EC%8D%AC-Python
+
+n = int(input())
+dp = []
+
+for i in range(n):
+    dp.append(list(map(int ,input().split())))
+
+for i in range(1, n):
+    for j in range(0, i+1):
+        # 왼쪽 끝
+        if j == 0:
+            dp[i][0] += dp[i-1][0]
+        # 오른쪽 끝
+        elif j == i:
+            dp[i][j] += dp[i-1][j-1]
+        # 나머지 위에서 2가닥 내려오는 부분들
+        else:
+            dp[i][j] += max(dp[i-1][j-1], dp[i-1][j])
+            
+print(max(dp[n-1]))
+'''
