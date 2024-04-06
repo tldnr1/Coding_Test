a822beaa91dbb99d047ba4f0c8c2392b7d3f1fdf
tldnr1 <tldnr1@knu.ac.kr>
2024-04-06 14:50:52 +0900

    new branch 'solvedAC'

--- Baekjoon/DP/9095.py
+++
@@ -1,9 +0,0 @@
-t = int(input())
-
-for _ in range(t):
-    n = int(input())
-    dp = [0, 1, 2, 4] + [0] * (n - 3)
-    for i in range(4, n+1):
-        dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
-
-    print(dp[n])
